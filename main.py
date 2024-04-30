6a3eb9082bca7fd7f3c4bf074865397e0a4c5bd8
behbod-eskafi <80491468+behbod-eskafi@users.noreply.github.com>
2024-04-30 08:08:20 +0330

    Completed the game.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+#Russian Roulette
+import random
 
+players = int(input("Number of players: "))
+#print(person)
+bullet = random.randint(1, players)
+# print(bullet)
+
+person = 0
+while True:
+  if bullet == person:
+    print(f"Palyer number {person}: you lost!")
+    break
+  person = int(input("Your number: "))
+  continue
